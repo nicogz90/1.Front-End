43b1a7b1efddea252588a2aeee5221161b3252d7
nicolasgonzalez <104945758+nicogz90@users.noreply.github.com>
2023-01-19 19:41:31 -0300

    Files relocation

--- Proyecto_Final/Parte1/js/ventas.js
+++
@@ -1,8 +0,0 @@
-// Carga de Tipo de Cambio:
-fetch("https://ha.edu.uy/api/rates")
-  .then(function (data) {
-    return data.json();
-  })
-  .then(function (data) {
-    document.querySelector("#rate span").textContent = data.uyu;
-  });
--- Proyecto_Final/Parte2/js/app.js
+++
@@ -1,2 +0,0 @@
-// Código JavaScript general para toda la aplicación.
-// En este ejemplo, este archivo queda vacío (se podría borrar).
